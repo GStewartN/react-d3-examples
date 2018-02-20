7227155ed0daeba67c3e8533616cf3e0fa9d5a1c
GStewartN <stewart.nathang@gmail.com>
2018-02-19 20:41:37 -0800

    Import data, tie dataHandling to App and display salaries loaded

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import * as d3 from 'd3';
+import _ from 'lodash';
 import Preloader from './components/Preloader';
+import {loadAllData} from './DataHandling';
 
 class App extends Component {
   state = {
-    techSalaries: []
+    techSalaries: [],
+    countyNames: [],
+    medianIncomes: []
+  }
+
+  componentWillMount() {
+    loadAllData(data => this.setState(data));
   }
 
   render() {
@@ -13,7 +22,8 @@ class App extends Component {
       );
     }
     return (
-      <div className='App'>
+      <div className='App Container'>
+        <h1>Loaded {this.state.techSalaries.length} salaries</h1>
       </div>
     );
   }
